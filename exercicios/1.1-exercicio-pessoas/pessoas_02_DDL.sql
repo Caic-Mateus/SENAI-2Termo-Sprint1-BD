8b3238389320a9c3df3f859391cfae8372f5310b
Caic Mateus <caicmateusfreitas@gmail.com>
2021-02-25 11:12:39 -0300

    feito até o exercicio 1.3 o excel

---
+++ exercicios/1.1-exercicio-pessoas/pessoas_02_DDL.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE Pessoas; --Criando Banco de Dados
+
+USE Pessoas;--Selecionando em qual os comandos serão executados
+
+CREATE TABLE CNH-- Criando tabela de CNH
+(
+	idCNH INT PRIMARY KEY IDENTITY-- idCNH é um int, chave primaria e é único
+);
+CREATE TABLE Email-- Criando tabela de Email
+(
+	idEmail INT PRIMARY KEY IDENTITY
+	
+)
+
