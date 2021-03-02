ae49a594240503a0e3d617033f4db3ebd1e46c86
Caic Mateus <caicmateusfreitas@gmail.com>
2021-03-02 15:20:00 -0300

    adiciona arquivo ddl e organiza em pastas

---
+++ projetoHROADS/scripts/hroads_01_DDL.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE SENAI_HROADS_TARDE
+
+USE SENAI_HROADS_TARDE
+
+CREATE TABLE Classe
+(
+	idClasse INT PRIMARY KEY IDENTITY ,
+	Nome VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE TipoHabilidade
+(
+	idTipoHabilidade INT PRIMARY KEY IDENTITY ,
+	Descricao VARCHAR (200) NOT NULL
+);
+CREATE TABLE Habilidade
+(
+	idHabilidade INT PRIMARY KEY IDENTITY ,
+	idTipoHabilidade INT FOREIGN KEY REFERENCES TipoHabilidade (idTipoHabilidade),
+	Nome VARCHAR (200) NOT NULL 
+);
+
+CREATE TABLE ClasseHabilidade
+(
+	idClasse INT FOREIGN KEY REFERENCES Classe(idClasse),
+	idHabilidade INT FOREIGN KEY REFERENCES Habilidade (idHabilidade)
+
+);
+CREATE TABLE Personagem 
+(
+	idPersonagem INT PRIMARY KEy IDENTITY,
+	idClasse INT FOREIGN  KEY REFERENCES Classe(idClasse),
+	Nome VARCHAR (200) NOT NULL,
+	MaxVida INT NOT NULL,
+	MaxMana INT NOT NULL,
+	DataAtt DATE NOT NULL,
+	DataCriacao DATE NOT NULL
+);
